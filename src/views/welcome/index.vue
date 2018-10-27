2ec348de1ff828eb0a81ecf0b87f3f01744af4a8
772184414@qq.com <Rabbit19870326>
2018-10-27 16:20:12 +0800

    新增welcome 首页

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -1,41 +1,36 @@
 <template>
   <div class="container">
-    <div class="el-row up" style="height: 60%;min-height: 60%;">
+    <div class="el-row up">
       <div class="admin-option">
         <div>
-          <img src="../../assets/welcome-images/admin.png" >
+          <img src="../../../static/welcome-images/admin.png" >
           <a @click="userInfo">系统管理员</a>
         </div>
         <div>
-          <img src="../../assets/welcome-images/pwd.png" >
+          <img src="../../../static/welcome-images/pwd.png" >
           <a @click="modifyPwd">修改密码</a>
         </div>
         <div>
-          <img src="../../assets/welcome-images/logout.png" >
-          <a @click="logout">注销登录</a>
+          <img src="../../../static/welcome-images/logout.png" >
+          <a @click="logout">{{ $t('navbar.logOut') }}</a>
         </div>
       </div>
       <div class="nav-tabs">
-        <div class="tab tab0 active">
-          <div class="title">基础平台</div>
-          <div class="subtitle">基础管理 区域服务</div>
-        </div>
-        <div class="tab tab1">
-          <div class="title">数据中心</div>
-          <div class="subtitle">数据分析 统计管理</div>
-        </div>
-        <div class="tab tab2">
-          <div class="title">业务应用</div>
-          <div class="subtitle">业务管理 应用中心</div>
-        </div>
-        <div class="tab tab3">
-          <div class="title">综合监督</div>
-          <div class="subtitle">综合服务 监督管理</div>
-        </div>
+        <a v-for="(p,index) in tabLists" :key="index" :class="p.selected ? 'active' : ''" class="tab" @click="changeActive(index)">
+          <div class="title">{{ p.title }}</div>
+          <div class="subtitle">{{ p.subtitle }}</div>
+        </a>
       </div>
     </div>
-    <div class="el-row down" style="height: 40%;min-height: 40%;">
-      &nbsp;
+    <div class="el-row down" >
+      <div v-for="(parent,pIndex) in tabLists" :key="pIndex" :class="parent.selected ? 'show' : 'hide'" class="parent">
+        <a v-for="(child,childIndex) in parent.children" :key="childIndex" class="child" @click="redirect(child.url)">
+          <div class="img">
+            <img :src="child.img" >&nbsp;
+          </div>
+          <div class="tb-title">{{ child.name }}&nbsp;</div>
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -45,8 +40,32 @@ export default {
   name: 'Welcome',
   data() {
     return {
-      activeName: 'tab1',
-      createdTimes: 0
+      tabLists: [
+        { title: '基础平台', subtitle: '基础管理 区域服务', selected: true, children:
+            [
+              { 'name': '基础注册', 'img': '../../../static/welcome-images/zc.png', url: 'dashboard' },
+              { 'name': '权限管理', 'img': '../../../static/welcome-images/qx.png', url: 'permission/page' }
+            ]
+        },
+        { title: '数据中心', subtitle: '数据分析 统计管理', selected: false, children:
+            [
+              // { 'name': '基础注册', 'img': 'zc.png' },
+              // { 'name': '权限管理', 'img': 'qx.png' }
+            ]
+        },
+        { title: '业务应用', subtitle: '业务管理 应用中心', selected: false, children:
+            [
+              // { 'name': '基础注册', 'img': 'zc.png' },
+              // { 'name': '权限管理', 'img': 'qx.png' }
+            ]
+        },
+        { title: '综合监督', subtitle: '综合服务 监督管理', selected: false, children:
+            [
+              // { 'name': '基础注册', 'img': 'zc.png' },
+              // { 'name': '权限管理', 'img': 'qx.png' }
+            ]
+        }
+      ]
     }
   },
   created() {
@@ -56,17 +75,34 @@ export default {
     // window.removeEventListener('hashchange', this.afterQRScan)
   },
   mounted() {
-    // this.refreshCode()
+    this.changeActive(0)
   },
   methods: {
     userInfo() {
-      alert('userInfo')
+      this.$store.dispatch('GetUserInfo').then(res => { // 拉取user_info
+        const roles = res.data.roles // note: roles must be a array! such as: ['editor','develop']
+        this.$store.dispatch('GenerateRoutes', { roles }).then(() => { // 根据roles权限生成可访问的路由表
+          this.$router.addRoutes(this.$store.getters.addRouters) // 动态添加可访问路由表
+          // this.$next() // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
+        })
+      })
     },
     modifyPwd() {
       alert('modify')
     },
     logout() {
-      alert('logout')
+      this.$store.dispatch('LogOut').then(() => {
+        location.reload()// In order to re-instantiate the vue-router object to avoid bugs
+      })
+    },
+    changeActive(index) {
+      for (var i = 0; i < this.tabLists.length; i++) {
+        this.tabLists[i].selected = false
+      }
+      this.tabLists[index].selected = true
+    },
+    redirect(url) {
+      this.$router.push({ path: url })
     }
   }
 }
@@ -80,7 +116,7 @@ export default {
     .up{
       height: 60%;
       min-height: 60%;
-      background-image: url("../../assets/welcome-images/up-bg.png");
+      background-image: url("../../../static/welcome-images/up-bg.png");
       background-size:100% 100%;
       .admin-option{
         text-align: right;
@@ -124,7 +160,7 @@ export default {
             font-size: 18px;
           }
         }
-        .tab0{
+        .tab:first-child{
           border-left: 1px solid #979bcd;
         }
         .tab.active{
@@ -136,6 +172,22 @@ export default {
     .down{
       height: 40%;
       min-height: 40%;
+      .show{
+        display: block;
+      }
+      .hide{
+        display: none;
+      }
+      .parent{
+        margin-left: 105px;
+        .child{
+          width: 150px;
+          margin-left: 120px;
+          margin-top: 100px;
+          float: left;
+          text-align: center;
+        }
+      }
     }
   }
 </style>
