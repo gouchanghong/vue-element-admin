771c02ccb68456aa2903f4f11331631b38e2d63a
gouchanghong@gmail.com <gch_19890130>
2018-10-29 15:54:54 +0800

    菜单栏调整，机构查询页面调整

--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -5,9 +5,9 @@
       :default-active="$route.path"
       :collapse="isCollapse"
       mode="vertical"
-      background-color="#304156"
-      text-color="#bfcbd9"
-      active-text-color="#409EFF"
+      background-color="#2E3D50"
+      text-color="#ADB8C1"
+      active-text-color="#FFFFFF"
     >
       <sidebar-item v-for="route in permission_routers" :key="route.path" :item="route" :base-path="route.path"/>
     </el-menu>
